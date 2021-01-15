9ab18ccfe9102b84953eb4fe4a3b26bbcd2a23c8
UmmeFarjana <77179668+UmmeFarjana@users.noreply.github.com>
2021-01-14 22:08:15 -0600

    Create stringsOperations.py

---
+++ 2-Data-Structures/stringsOperations.py
@@ -0,0 +1,24 @@
+str1 = "facebook"
+print (len(str1))
+
+print(str1.lower())
+
+print(str1.upper())
+
+print(str1[1:6])
+
+print(str1[6:])
+print(str1[:6])
+
+print(str1[1:])
+
+print(str1[-5:-1])
+
+str2 =" -Facebook- "
+print(str2.strip())
+
+
+print(str1.replace("k"," "))
+
+str3 = "Apple, Bee, Honey"
+print(str3.split(","))
