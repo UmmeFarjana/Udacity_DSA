fe2a156129e391d9a8ef299d09aa3f410e8e8a54
UmmeFarjana <77179668+UmmeFarjana@users.noreply.github.com>
2021-01-22 23:21:48 -0600

    Update reversing_string.py

--- 2-Data-Structures/reversing_string.py
+++ 2-Data-Structures/reversing_string.py
@@ -1,1 +1,9 @@
 
+def reverse(string):
+    reversed_string = ""
+    for i in string:
+        reversed_string = i + reversed_string
+    return reversed_string
+
+string = "water"
+reverse(string)
